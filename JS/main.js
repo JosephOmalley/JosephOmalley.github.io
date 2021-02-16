71c6bb82a2c98a03e5f54cd5b457480c7cd1d62e
Joe <josephomalley135@gmail.com>
2021-02-15 21:49:42 -0600

    Add JavaScript to portfolio
    add flip-able project cards
    add typewriter effect on page 3

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+var i = 0;
+var txt = ' me by filling out the info below';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+
+const card = document.querySelectorAll('.card-inner');
+card.forEach((card) => {
+    card.addEventListener('click', function() {
+        card.classList.toggle('is-flipped')
+    }); 
+});
+
+
